9a88e2bea42277aab3ccf1717855b2bb98b6b94c
LeivaDiego <lei21752@uvg.edu.gt>
2025-06-04 22:59:00 -0600

    feat: updated layout with new sections

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,8 @@
 import { GlobalStyles } from '../../styles/GlobalStyles'
 import Home from '../sections/Home'
-
+import Projects from '../sections/Projects'
+import Skills from '../sections/Skills'
+import Contact from '../sections/Contact'
 
 const Layout = () => {
   return (
@@ -8,7 +10,9 @@ const Layout = () => {
       <GlobalStyles />
       <main>
         <Home />
-
+        <Projects />
+        <Skills />
+        <Contact />
       </main>
     </>
   )
